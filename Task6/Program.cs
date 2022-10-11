51ab4c4935475357706c485da7836a597e7c3550
Anastasya <okayota83@mail.ru>
2022-10-11 20:20:11 +0400

    add Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,43 @@
+﻿// Найти произведение пар чисел в одномерном массиве. 
+
+
+void FillArray(int[] array, int min, int max)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+Console.WriteLine("Задайте длину массива");
+int n = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Задайте диапазон чисел массива [min,max]");
+Console.WriteLine("Введите min");
+int min = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите max");
+int max = int.Parse(Console.ReadLine() ?? "0");
+int [] array = new int[n];
+
+FillArray(array, min, max);
+PrintArray (array);
+Console.WriteLine();
+Console.Write("Произведения парных чисел массива: ");
+int product = 0;
+for (int i = 0; i < n/2; i++)
+{ 
+    product = array[i]*array[n-1-i];
+    Console.Write($"{product} ");
+}
+   
+
+
+
+
+
