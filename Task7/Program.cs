ae78685163e0656308aa4dd2f21d8f176d1bd097
Anastasya <okayota83@mail.ru>
2022-10-11 21:32:34 +0400

    add Task7

---
+++ Task7/Program.cs
@@ -0,0 +1,25 @@
+﻿// В Указанном массиве вещественных чисел найдите 
+// разницу между максимальным и минимальным элементом
+void PrintArray (double[] array)
+{
+   for (int i = 0; i < array.Length; i++)
+    Console.Write($"{array[i]}| ");
+}
+double [] array = {5.2, 3.3, -2.8, -3.7, 0.25};
+double max = array[0];
+double min = array[0];
+
+for (int i = 0; i < array.Length; i++)
+{
+  if (array[i]>max) max = array[i];
+}
+for (int i = 0; i < array.Length; i++)
+{
+  if (array[i]<min) min = array[i];
+}
+
+double result = max-min;
+Console.WriteLine("Ваш массив:");
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine ($"Разница между максимальным и минимальным элементами массива |{result}|");
